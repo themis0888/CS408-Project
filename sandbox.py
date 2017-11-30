b8d7beacfb363380aa9df936969eedc130a28616
themis0888 <themis0888@gmail.com>
2017-12-01 04:05:25 +0900

    preprocessor and training data added

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,4 @@
-#import cv2
+import cv2
 import sys
 import os
 import SAD
@@ -13,34 +13,21 @@ import numpy as np
 from scipy import signal
 from scipy import misc
 
-source = Image.open("test_bg.jpg")
-temp = Image.open("cup.jpg")
-source = source.convert('L')
-temp  = temp.convert('L')
-#face = np.array(source.getdata(),np.uint8) #.reshape(source.size)
-    #+np.array(source.getdata())[:,1].reshape(source.size)
-    #+np.array(source.getdata())[:,2].reshape(source.size))
-face = np.asarray(source)
-template = np.asarray(temp)
-face = face - face.mean()
-#template = np.copy(face[300:365, 670:750])  # right eye
-template = template - face.mean()
-template = template - template.mean()
-face = face + np.random.randn(*face.shape) * 5  # add noise
-corr = signal.correlate2d(face, template, boundary='symm', mode='same')
-y, x = np.unravel_index(np.argmax(corr), corr.shape)  # find the match
 
-import matplotlib.pyplot as plt
-fig, (ax_orig, ax_template, ax_corr) = plt.subplots(3, 1,
-                                                    figsize=(6, 15))
-ax_orig.imshow(face, cmap='gray')
-ax_orig.set_title('Original')
-ax_orig.set_axis_off()
-ax_template.imshow(template, cmap='gray')
-ax_template.set_title('Template')
-ax_template.set_axis_off()
-ax_corr.imshow(corr, cmap='gray')
-ax_corr.set_title('Cross-correlation')
-ax_corr.set_axis_off()
-ax_orig.plot(x, y, 'ro')
-fig.show()
+
+
+def video_to_frames(video, path_output_dir):
+    vidcap = cv2.VideoCapture(video)
+    count = 0
+    while vidcap.isOpened():
+        success, image = vidcap.read()
+        if success:
+            # Capture Every 10 frame
+            if(count % 10 == 0):
+                cv2.imwrite(os.path.join(path_output_dir, '%d.png') % count, image)
+            count += 1
+        else:
+            break
+    cv2.destroyAllWindows()
+    vidcap.release()
+    return count
---
+++ training_data/preprocessor.py
@@ -0,0 +1,93 @@
+import cv2
+import sys
+import os
+import SAD
+import numpy as np
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+from config import CONFIG
+from scipy.misc import imshow
+import numpy as np
+from config import CONFIG
+from os import walk
+
+from scipy import signal
+from scipy import misc
+
+
+
+name_list = ['cup', 'glasscase', 'pencilcase', 'rice', 'scissors', 'shave', 'snack', 'socks', 'spaghetti', 'tape']
+def video_to_frames(video, path_output_dir, cnt = 0):
+    vidcap = cv2.VideoCapture(video)
+    count = cnt
+    while vidcap.isOpened():
+        success, image = vidcap.read()
+        if success:
+            # Capture Every 10 frame
+            if(count % 1 == 0):
+                cv2.imwrite(os.path.join(path_output_dir, '{0:03d}.jpg'.format(count)), image)
+            count += 1
+        else:
+            break
+    cv2.destroyAllWindows()
+    vidcap.release()
+    return count
+
+"""
+for i in range(len(name_list)):
+    f = []
+    filenames = []
+    for (dirpath, dirnames, filenames) in walk(str(i+1) + '_' + name_list[i]+'/'):
+        print(str(i+1) + '_' + name_list[i]+'/')
+        f.extend(filenames)
+    c = 0
+    for name in filenames:
+        c = video_to_frames(str(i+1) + '_' + name_list[i]+'/'+name, str(i+1) + '_' + name_list[i], cnt = c)
+
+
+"""
+for i in range(len(name_list)):
+    f = []
+    filenames = []
+    for (dirpath, dirnames, filenames) in walk(str(i+1) + '_' + name_list[i]+'/'):
+        print(str(i+1) + '_' + name_list[i]+'/')
+        f.extend(filenames)
+    c = 0
+    for name in filenames:
+        if 'mp4' in name:
+            continue
+        im2 = Image.open(str(i+1) + '_' + name_list[i]+'/'+name)
+
+        # select the template imabe from current image. 
+        im1 = im2.crop((int(im2.size[0]/2-im2.size[1]/2),0,int(im2.size[0]/2+im2.size[1]/2),im2.size[1]))
+
+        # shrink the image for operation speed, and this still not affect to the performance
+        # Here!!!! you can resize this part!!!!! 
+        ratio = 0.3
+        im = im1.resize( [int(ratio * s) for s in im1.size] )
+        im.save(str(i+1) + '_' + name_list[i]+'/'+name)
+
+
+"""
+f = []
+for (dirpath, dirnames, filenames) in walk('COCA/'):
+    f.extend(filenames)
+
+for name in filenames:
+    im
+
+im2 = Image.open("toimage/110.png")
+
+# select the template imabe from current image. 
+im1 = im2.crop((0,0.15*im2.size[1],0.3*im2.size[0],0.85*im2.size[1]))
+ratio = 0.3
+
+#searchImage1 = Image.open("test_bg.jpg")
+#im1 = Image.open("cup.jpg")
+
+
+# shrink the image for operation speed, and this still not affect to the performance
+searchImage = searchImage1.resize( [int(ratio * s) for s in searchImage1.size] )
+
+"""
